5bcd76a354597fcede5f86bdb6da988a4d63aeeb
Arijeet2303 <143900749+Arijeet2303@users.noreply.github.com>
2024-09-15 11:38:03 +0530

    first commit

---
+++ BF/src/main/java/com/cloth/BF/BfApplication.java
@@ -0,0 +1,13 @@
+package com.cloth.BF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BfApplication.class, args);
+	}
+
+}
---
+++ BF/src/test/java/com/cloth/BF/BfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloth.BF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
